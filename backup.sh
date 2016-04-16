05852f6bae4e2e42578455020271a503ea009cca
Georgi Tsaklev <y0c0vc@gmail.com>
2016-04-16 12:49:21 +0100

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo Dumping database
+CONFIGFILE=/app/bashdump/.dropbox_uploader
+mongodump
+NEWFILE=dump`date +%Y-%m-%d`.tar.gz
+echo Compressing folder to $NEWFILE
+tar -zcvf $NEWFILE dump
+echo Uploading to DropBox
+./dropbox_uploader.sh -f $CONFIGFILE upload $NEWFILE /
+echo Removing temporary data
+rm -rf dump
+rm $NEWFILE
+echo Removing old backups
+TOTAL=0
+DUMPS=`./dropbox_uploader.sh -f $CONFIGFILE list | awk '{print $3}' | grep dump | sort -r`
+for i in $DUMPS
+do
+    if [[ "$TOTAL" == 3 ]]
+    then
+        echo Deleting $i
+        ./dropbox_uploader.sh -f $CONFIGFILE delete /$i
+    else
+        TOTAL=$((TOTAL + 1))
+    fi
+done
+echo Done
